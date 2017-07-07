e16a89353c350052df02298055660936a2f40a74
Michael Schneider <mischneider1@gmail.com>
2017-07-07 09:06:18 -0700

    Add some documentation

--- modules/createSubscriber.js
+++ modules/createSubscriber.js
@@ -2,6 +2,11 @@ import invariant from 'invariant'
 import React from 'react'
 import PropTypes from 'prop-types'
 
+/**
+ * Higher order component that takes in a channel name and BaseComponent and
+ * returns a <Subscriber> which pulls the value for a channel off of
+ * context.broadcasts and passes it as prop down to it's BaseComponent.
+ */
 const createSubscriber = channel => BaseComponent => {
   return class Subscriber extends React.Component {
     static contextTypes = {
