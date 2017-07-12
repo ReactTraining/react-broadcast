e3cc5644a1c0856c139225702e5f954a4361e36a
MICHAEL JACKSON <mjijackson@gmail.com>
2017-07-11 21:27:01 -0700

    Fix UMD build file names

--- tools/build.js
+++ tools/build.js
@@ -25,20 +25,20 @@ exec('babel modules -d es --ignore __tests__', {
   BABEL_ENV: 'es'
 })
 
-console.log('\nBuilding react-media.js ...')
+console.log('\nBuilding react-broadcast.js ...')
 
-exec('webpack modules/index.js umd/react-media.js', {
+exec('webpack modules/index.js umd/react-broadcast.js', {
   NODE_ENV: 'production'
 })
 
-console.log('\nBuilding react-media.min.js ...')
+console.log('\nBuilding react-broadcast.min.js ...')
 
-exec('webpack -p modules/index.js umd/react-media.min.js', {
+exec('webpack -p modules/index.js umd/react-broadcast.min.js', {
   NODE_ENV: 'production'
 })
 
 const size = gzipSize.sync(
-  fs.readFileSync('umd/react-media.min.js')
+  fs.readFileSync('umd/react-broadcast.min.js')
 )
 
 console.log('\ngzipped, the UMD build is %s', prettyBytes(size))
